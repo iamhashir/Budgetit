83c9cea6ba473abd28bc9e31d018ad2dc00f61ed
iamhashir <72984973+iamhashir@users.noreply.github.com>
2026-09-01 02:31:12 +0400

    Add token barrel exports

---
+++ src/theme/tokens/index.ts
@@ -0,0 +1,19 @@
+export { components, type ComponentTokens } from './components';
+export { motion, type MotionTokens } from './motion';
+export {
+  accessibility,
+  borderWidths,
+  primitiveColors,
+  primitives,
+  radii,
+  spacing,
+  typography,
+  type RadiusToken,
+  type SpacingToken,
+  type TypographyToken,
+} from './primitives';
+export {
+  semanticColors,
+  type ColorMode,
+  type SemanticColors,
+} from './semantic';
